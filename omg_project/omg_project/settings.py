b1ff00a90d7c4bbf871a643e718dbb40ea5170d0
RoxyYujinKim <yujinkim0707@gmail.com>
2023-10-18 13:22:36 +0900

    Conf : postgresql, rds DB 설정

--- omg_project/omg_project/settings.py
+++ omg_project/omg_project/settings.py
@@ -75,11 +75,16 @@ WSGI_APPLICATION = 'omg_project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+db = json.load(open(os.path.join(SECRETS_DIR, 'db.json')))
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': db['DATABASE'],
+        'USER': db['USER'],
+        'PASSWORD': db['PASSWORD'],
+        'HOST': db['HOST'],
+        'PORT': db['PORT'],
     }
 }
 
