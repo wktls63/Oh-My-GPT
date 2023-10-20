44dc0fe6196a264439eecc52642a5feb6b00e906
kyungmin <wktls63@naver.com>
2023-10-20 17:25:20 +0900

    ADD 결제완료 시리얼라이져 추가

--- omg_project/omg_app/serializers.py
+++ omg_project/omg_app/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework                       import serializers
-from .models                              import SubscriptionProduct
+from .models                              import SubscriptionProduct, Payment
 
 
 class SubscriptionSerializer(serializers.ModelSerializer):
@@ -9,3 +9,10 @@ class SubscriptionSerializer(serializers.ModelSerializer):
     class Meta:
         model = SubscriptionProduct
         fields = '__all__'
+
+
+class PaymentSerializer(serializers.ModelSerializer):
+
+    class Meta: 
+        model = Payment
+        fields = "__all__"
