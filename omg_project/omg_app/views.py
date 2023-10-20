ba531b8939b7d4122f7717341cbd387bcb0bd4c9
MinoKimm <jyys0531@gmail.com>
2023-10-20 17:32:23 +0900

    [Feat] update e-mail auth, login/logout, signin

--- omg_project/omg_app/views.py
+++ omg_project/omg_app/views.py
@@ -1,13 +1,13 @@
-from django.shortcuts           import render
+from django.shortcuts           import render ,get_object_or_404
 from rest_framework             import status
 from rest_framework.views       import APIView
 from rest_framework.response    import Response
-from .models                    import SubscriptionProduct
-from .serializers               import SubscriptionSerializer
+from .models                    import SubscriptionProduct, Payment, User
+from .serializers               import SubscriptionSerializer ,PaymentSerializer
 
 
 # API View
-class SubScriptionAPIViewp(APIView):
+class SubScriptionAPIView(APIView):
     """
     구독 서비스 상품 목록 API
     """
@@ -18,13 +18,24 @@ class SubScriptionAPIViewp(APIView):
         serializer = SubscriptionSerializer(queryset, many=True)
 
         return Response(serializer.data, status=status.HTTP_200_OK)
+    
+class PaymentValidationView(APIView):
+
+    def post(self, request, **kwards):
+
+        serializer                  = PaymentSerializer(data=request.data)
+
+        if serializer.is_valid(raise_exception=True):
+
+            serializer.save()
+
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 def index(request):
     return render(request, 'index.html')
-
-def chat(request):
-    return render(request, 'chat.html')
 
 def write(request):
     return render(request, 'write.html')
