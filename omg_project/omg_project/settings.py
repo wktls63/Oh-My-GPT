756303fecdd37800e99756863b3e4f847fff5d49
Ethanall94 <Ethanall94@gmail.com>
2023-10-25 00:14:28 +0900

    Update : email verify

--- omg_project/omg_project/settings.py
+++ omg_project/omg_project/settings.py
@@ -204,3 +204,15 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # 비동기 async 함수에서 ORM 조회 설정
 os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = 'true'
+
+
+# mail 인증
+mail = json.load(open(os.path.join(SECRETS_DIR, 'mail.json')))
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = mail['EMAIL_HOST_USER']
+EMAIL_HOST_PASSWORD = mail['EMAIL_HOST_PASSWORD']
+DEFAULT_FROM_MAIL = EMAIL_HOST_USER
