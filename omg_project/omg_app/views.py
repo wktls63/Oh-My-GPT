b6522b4d432a809e55f3af7e7d0236937a66936a
kyungmin <wktls63@naver.com>
2023-10-24 22:48:10 +0900

    ADD 결제시 유저 구독 정보 업데이트

--- omg_project/omg_app/views.py
+++ omg_project/omg_app/views.py
@@ -34,44 +34,100 @@ class SubScriptionAPIView(APIView):
         return Response(serializer.data, status=status.HTTP_200_OK)
     
 class PaymentValidationView(APIView):
+    """
+    결제 요청 POST를 받으면 결제를 진행하는 API
+    """
 
     def post(self, request, **kwards):
 
-        # 사용자 정보 가져오기
-        access_token = request.COOKIES.get('access')
-        refresh_token = request.COOKIES.get('refresh')    
+        # JWT 토큰에서 유저 정보 가져오기
+        access_token = request.COOKIES.get('access')   
         payload = jwt.decode(access_token, SECRET_KEY, algorithms='HS256')
-
         user = User.objects.get(id=payload["user_id"])
 
-        data = {
-            "merchant_id" : request.data["merchant_id"],
-            "amount" : request.data["amount"],
-            "subscription_product_id" : request.data["subscription_product_id"],
-            "user_id" : user.id
-        }
+        # 유저 정보에서 해당 유저의 결제 내역을 조회
+        user_payment_object = Payment.objects.filter(user_id=user.id).first()
 
+        # 결제시 requset 받는 데이터
+        data = {
+                "merchant_id" : request.data["merchant_id"],
+                "amount" : request.data["amount"],
+                "subscription_product" : request.data["subscription_product_id"],
+                "payment_status" : request.data["status"],
+                "user" : user.id
+            }
+  
+        payment_amount = request.data['amount']
+        user_subscription_item = request.data["subscription_product_id"]
+
+        # 시리얼 라이저에 request 데이터 전달
         serializer = PaymentSerializer(data=data)
 
-        if serializer.is_valid(raise_exception=True):
+        # 유저가 결제내역이 없다면 결제를 진행
+        if user_payment_object is None:
 
-            serializer.save()
+            if serializer.is_valid(raise_exception=True):
 
-            return Response(serializer.data, status=status.HTTP_200_OK)
+                # 유저 구독 상품을 GPT Pro 로 업데이트
+                if user_subscription_item == 1:
 
+                    user.payment_status = User.PAYMENT_PRO
+
+                    user.save()
+
+                    serializer.save()
+
+                    return Response(serializer.data, status=status.HTTP_200_OK)
+
+                # 유저 구독 상품을 GPT Pro Plus 로 업데이트
+                elif user_subscription_item == 2:
+
+                    user.payment_status = User.PAYMENT_PRO_PLUS
+
+                    user.save()
+
+                    serializer.save()
+
+                    return Response(serializer.data, status=status.HTTP_200_OK)
+
+                # 유저 구독 상품을 GPT Pro Enterprise 로 업데이트
+                elif user_subscription_item == 3:
+
+                    user.payment_status = User.PAYMENT_ENTERPRISE
+
+                    user.save()
+
+                    serializer.save()
+
+                    return Response(serializer.data, status=status.HTTP_200_OK)
+
+                else:
+
+                    user.payment_status = User.PAYMENT_BASIC
+
+                    user.save()
+
+                    serializer.save()
+
+                    return Response(serializer.data, status=status.HTTP_200_OK)
+            
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+        # 유저가 똑같은 상품의 결제 내역이 있으면
+        elif user_payment_object.amount == payment_amount:
+
+            serializer.is_valid(raise_exception=True)
+
+            return Response(serializer.errors, status=status.HTTP_423_LOCKED)
+
+        # 유저가 다른 상품의 결제 내역이 있으면
+        elif user_payment_object.amount != payment_amount:
+
+            serializer.is_valid(raise_exception=True)
+
+            return Response(serializer.errors, status=status.HTTP_418_IM_A_TEAPOT)
+        
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-
-        # serializer                  = PaymentSerializer(data=request.data)
-
-        # if serializer.is_valid(raise_exception=True):
-
-        #     serializer.save()
-
-        #     return Response(serializer.data, status=status.HTTP_200_OK)
-
-        # return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 def index(request):
