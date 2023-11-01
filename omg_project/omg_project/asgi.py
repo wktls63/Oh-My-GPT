a83abb04503cbee81f5ef099f50d48016bd0fbac
RoxyYujinKim <yujinkim0707@gmail.com>
2023-11-01 17:24:33 +0900

    Refactor : 테스트 코드 실행을 위해 asgi 리팩토링

--- omg_project/omg_project/asgi.py
+++ omg_project/omg_project/asgi.py
@@ -1,26 +1,25 @@
 import os
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "omg_project.settings")  # 경로 수정
+
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
-from omg_project.channels_middleware import ChannelsJWTAuthMiddleware  # 추가된 부분
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
-# Initialize Django ASGI application early to ensure the AppRegistry
-# is populated before importing code that may import ORM models.
 django_asgi_app = get_asgi_application()
 
-import omg_app.routing as routing
+def get_application():
+    import omg_app.routing as routing
 
-application = ProtocolTypeRouter(
-    {
-        "http": django_asgi_app,
-        "websocket": AllowedHostsOriginValidator(
-            AuthMiddlewareStack(URLRouter(routing.websocket_urlpatterns))
-            # ChannelsJWTAuthMiddleware(  # 이곳에 추가된 부분
-            #     AuthMiddlewareStack(URLRouter(routing.websocket_urlpatterns))
-            # )
-        ),
-    }
-)
+    application = ProtocolTypeRouter(
+        {
+            "http": django_asgi_app,
+            "websocket": AllowedHostsOriginValidator(
+                AuthMiddlewareStack(URLRouter(routing.websocket_urlpatterns))
+            ),
+        }
+    )
+    return application
+
+application = get_application()
