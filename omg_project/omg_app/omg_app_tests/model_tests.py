f1cf88d035cf82546f6626d0b7002a0beb4256d2
JunHyung <81272766+hyung000620@users.noreply.github.com>
2023-11-01 14:01:36 +0900

    Add : email test

--- omg_project/omg_app/omg_app_tests/model_tests.py
+++ omg_project/omg_app/omg_app_tests/model_tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
+from rest_framework.test import APIRequestFactory
+from ..omg_app_views.model_views import EmailAPIView
+from django.core import mail
 
-# Create your tests here.
+class EmailAPIViewTest(TestCase):
+
+    def setUp(self):
+        self.factory = APIRequestFactory()
+        self.url = '/api/email/'
+        self.view = EmailAPIView.as_view()
+
+    def test_send_email_valid_data(self):
+        data = {
+            'email_title': 'Test Email',
+            'email_msg': 'Hello, this is a test email',
+            'email': 'dksms1@naver.com'
+        }
+        request = self.factory.post(self.url, data)
+        response = self.view(request)
+
+        self.assertEqual(len(mail.outbox), 1)
+        self.assertEqual(mail.outbox[0].subject, 'Test Email')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data['status'], 'success')
+
+    def test_send_email_invalid_data(self):
+        data = {}
+        request = self.factory.post(self.url, data)
+        response = self.view(request)
+        self.assertEqual(response.status_code, 400)
